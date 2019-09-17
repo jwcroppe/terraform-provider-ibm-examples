9a3d2068797df92995ff340100092568a09b9793
Joe Cropper <jwcroppe@us.ibm.com>
2019-09-17 07:45:05 -0500

    Update provider.tf

--- simple-vm-power-vs/provider.tf
+++ simple-vm-power-vs/provider.tf
@@ -1,4 +1,6 @@
 provider "ibm" {
+    # Temporary hack to force WIP Terraform provider to be used
+    version          = ">= 0.17.1, <= 0.17.1"
     ibmcloud_api_key = "${var.ibmcloud_api_key}"
     region           = "${var.ibmcloud_region}"
 }
