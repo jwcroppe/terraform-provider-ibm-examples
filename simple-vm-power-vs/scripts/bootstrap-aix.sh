60cc6b27fb08ece6e6386b959bd4a1c66b4b63ef
Joe Cropper <jwcroppe@us.ibm.com>
2019-09-19 22:54:03 -0500

    Update bootstrap-aix.sh

--- simple-vm-power-vs/scripts/bootstrap-aix.sh
+++ simple-vm-power-vs/scripts/bootstrap-aix.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Installs MySQL and sets up an initial password; feel free to change
-
-MYSQL_PASSWD="s3cur3Pa5sw0rd"
 yum install -y MySQL.ppc
-/opt/freeware/bin/mysqladmin -u root password "$MYSQL_PASSWD"
-/opt/freeware/bin/mysql --user="root" --password="$MYSQL_PASSWD" --database="mysql" --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';
+/opt/freeware/bin/mysqladmin -u root password 's3cur3Pa5sw0rd'
+/opt/freeware/bin/mysql --user="root" --password="s3cur3Pa5sw0rd" --database="mysql" --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';"
