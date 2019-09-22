4ceb47997720508ea73e3825acd2795735afaf34
Joe Cropper <jwcroppe@us.ibm.com>
2019-09-22 00:46:13 -0500

    Update bootstrap-aix.sh

--- simple-vm-power-vs/scripts/bootstrap-aix.sh
+++ simple-vm-power-vs/scripts/bootstrap-aix.sh
@@ -9,7 +9,7 @@ chfs -a size=+2G /usr
 # Install MySQL
 yum install -y MySQL.ppc
 /opt/freeware/bin/mysqladmin -u root password 's3cur3Pa5sw0rd'
-/opt/freeware/bin/mysql --user="root" --password="s3cur3Pa5sw0rd" --database="mysql" --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';"
+/opt/freeware/bin/mysql --user="root" --password="s3cur3Pa5sw0rd" --database="mysql" --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 
 # Load some test data in the database
 yum update -y curl
